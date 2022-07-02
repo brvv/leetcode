d77c657faf5174334c84e149e6a48f1cc7bbfd07
brvv <vladislav.brovikov@gmail.com>
2022-07-02 06:54:05 -0400

    Solution for binary-tree-right-side-view done on 02-Jul-2022, at 06:54:05

---
+++ 2022/Jul/02nd, at 06 54 - binary tree right side view.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        depth = 0
+        levels = []
+        current_nodes = []
+        current_nodes.append(root)
+        next_nodes = []
+        
+        
+        
+        while current_nodes:
+            level = []
+            
+            for node in current_nodes:
+                if not node:
+                    continue
+                
+                level.append(node.val)
+                
+                if node.left:
+                    next_nodes.append(node.left)
+                if node.right:
+                    next_nodes.append(node.right)
+                    
+            current_nodes = next_nodes.copy()
+            next_nodes = []
+            levels.append(level)
+        res = []
+        for val in levels:
+            res.append(val[-1])
+        
+        return res
+                    
+        
