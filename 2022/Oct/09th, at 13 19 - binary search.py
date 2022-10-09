696bedc8547f0cb29fe5062f3d5dc3c362586732
brvv <vladislav.brovikov@gmail.com>
2022-10-09 13:19:02 -0400

    Solution for binary-search done on 09-Oct-2022, at 13:19:02

---
+++ 2022/Oct/09th, at 13 19 - binary search.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start=0
+        end=len(nums) - 1
+
+        
+        while start <= end:
+            middle = (start + end) // 2
+            if nums[middle] < target:
+                start = middle + 1
+            elif nums[middle] > target:
+                end = middle - 1
+            else:
+                return middle
+        return -1
+            
