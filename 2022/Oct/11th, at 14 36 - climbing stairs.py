c043237f8c79facf3128db036a81284b3cb5d902
brvv <vladislav.brovikov@gmail.com>
2022-10-11 14:36:26 -0400

    Solution for climbing-stairs done on 11-Oct-2022, at 14:36:26

---
+++ 2022/Oct/11th, at 14 36 - climbing stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        lst = [None for _ in range(n+1)]
+        
+        lst[-1] = 1
+        lst[-2] = 1
+        
+        for i in range(n - 2, -1, -1):
+            lst[i] = lst[i + 1] + lst[i + 2]
+        return lst[0]
