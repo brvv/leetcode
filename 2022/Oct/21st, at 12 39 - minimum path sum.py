e9758757319fcbcb85f41e747d53ac54bd1815b1
brvv <vladislav.brovikov@gmail.com>
2022-10-21 12:39:30 -0400

    Solution for minimum-path-sum done on 21-Oct-2022, at 12:39:30

---
+++ 2022/Oct/21st, at 12 39 - minimum path sum.py
@@ -0,0 +1,66 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        # recursive with memoization:
+        cache = {}
+        def minPathSumRecursive(row_i=0, col_i=0):
+            current_cost = grid[row_i][col_i]
+            
+            if row_i == len(grid) - 1 and col_i == len(grid[0]) - 1:
+                return current_cost
+            
+            if (tuple([row_i, col_i]) in cache):
+                return cache[tuple([row_i, col_i])]
+
+            bottom = 10000000
+            right = 10000000
+
+            if row_i + 1 < len(grid):
+                bottom = current_cost + minPathSumRecursive(row_i + 1, col_i)
+
+            if col_i + 1 < len(grid[0]):
+                right = current_cost + minPathSumRecursive(row_i, col_i + 1)
+
+            cache[tuple([row_i, col_i])] = min(bottom, right)
+            return cache[tuple([row_i, col_i])]
+        
+        # 2d table dp
+        def minPathSum2DTable():
+            table = [[0 for item in row] for row in grid]
+            
+            for row_i, row in enumerate(table):
+                for col_i, item in enumerate(row):
+                    cost = grid[row_i][col_i]
+                    
+                    if row_i == 0 and col_i == 0:
+                        table[row_i][col_i] = cost
+                    else:
+                        top = table[row_i-1][col_i] if row_i - 1 >= 0 else 10000000000000
+                        left = table[row_i][col_i - 1] if col_i - 1 >= 0 else 10000000000000
+                        
+                        table[row_i][col_i] = cost + min(top, left)
+                        
+                        
+            return table[-1][-1]
+        
+        # two rows dp
+        def minPathSumRow():
+            row = [10000000000000 for item in grid[0]]
+            
+            for row_i, grid_row in enumerate(grid):
+                new_row = [0 for item in grid[0]]
+                for col_i, item in enumerate(grid_row):
+                    cost = grid[row_i][col_i]
+                    
+                    if row_i == 0 and col_i == 0:
+                        new_row[col_i] = cost
+                    else:
+                        top = row[col_i]
+                        left = new_row[col_i - 1] if col_i - 1 >= 0 else 10000000000000
+                        
+                        new_row[col_i] = cost + min(top, left)
+                row = new_row
+                        
+            return row[-1]
+                    
+        
+        return minPathSumRow()
