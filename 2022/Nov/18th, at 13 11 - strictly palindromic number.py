be34a49fe6fe5102850889a9845a007857eaf174
brvv <vladislav.brovikov@gmail.com>
2022-11-18 13:11:57 -0500

    Solution for strictly-palindromic-number done on 18-Nov-2022, at 13:11:57

---
+++ 2022/Nov/18th, at 13 11 - strictly palindromic number.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        def isPalindrome(s):
+            start = 0
+            end = len(s) - 1
+            while start <= end:
+                chr1 = s[start]
+                chr2 = s[end]
+                
+                if chr1 != chr2:
+                    return False
+                
+                start += 1
+                end -= 1
+            return True
+        
+        def getNumInBase(n, base):
+            if n == 0:
+                return '0'
+            res = ""
+            
+            while n > 0:
+                n, r = divmod(n, base)
+                res += str(r)
+            return res
+        
+        res = True
+        for base in range(2, n - 1):
+            res = res and isPalindrome(getNumInBase(n, base))
+        return res
