b516c551d811e103c7b4db0f1144b21f9b3fe2ec
brvv <vladislav.brovikov@gmail.com>
2022-12-30 21:01:45 -0500

    Solution for global-and-local-inversions done on 30-Dec-2022, at 21:01:45

---
+++ 2022/Dec/30th, at 21 01 - global and local inversions.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isIdealPermutation(self, nums: List[int]) -> bool:
+        #prove all global inversions are local inversions
+        #prove there are no global inversions (that are not local inversions)
+
+        if len(nums) < 3:
+            return True
+
+        smallest_right = [None for i in nums]
+        smallest_right[-1] = nums[-1]
+
+        for i in range(len(nums) - 2, -1 , -1):
+            smallest_right[i] = min(smallest_right[i + 1], nums[i])
+
+        
+        for i in range(len(nums) - 3, -1 , -1):
+            if nums[i] > smallest_right[i + 2]:
+                return False
+        return True
